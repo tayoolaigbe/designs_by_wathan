a3e8ef7a1563e512fef4f6e1a9c9b1e1e15dbe84
Eyitayo <lohmanlaurie86@gmail.com>
2023-11-29 10:52:12 +0000

    Fixed the tailwindcss/forms error

--- workcation/tailwind.config.js
+++ workcation/tailwind.config.js
@@ -1,8 +1,9 @@
+import vitePluginRequire from 'vite-plugin-require';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [vitePluginRequire('@tailwindcss/custom-forms')],
 };
